9c629c85ed82a33735a69804975f92a7c5b2bd66
Hector1527 <hectorperezgonzalez09@gmail.com>
2023-11-11 16:57:29 -0600

    Ejercicio 2

--- PAT_Parcial_Reposicion/Ejercicio01.cpp
+++ PAT_Parcial_Reposicion/Ejercicio01.cpp
@@ -1,6 +1,37 @@
 #include "Ejercicio01.h"
 
+//Node<int>* addTwoNumbersHelper(Node<int>* num1, Node<int>* num2, int sizeL1, int sizeL2, int& acarreo)
+//{
+//    if (sizeL1 == 0 && sizeL2 == 0 && acarreo == 0)
+//        return 0;
+//
+//    int valor1 = 0;
+//    int valor2 = 0; 
+//
+//    if (num1 && num2)
+//    {
+//        valor1 = num1->value;
+//        valor2 = num2->value;
+//    }
+//    int sum = valor1 + valor2 + acarreo;
+//    acarreo = sum / 10;
+//
+//    Node<int>* result = new Node<int>();
+//    result->value = sum % 10;
+//
+//    if (num1)
+//        num1 = num1->next;
+//    if (num2)
+//        num2 = num2->next;
+//
+//    result->next = addTwoNumbersHelper(num1, num2, sizeL1 - 1, sizeL2 - 1, acarreo);
+//
+//    return result;
+//}
+
 Node<int>* Ejercicio01::addTwoNumbers(Node<int>* l1, int sizeL1, Node<int>* l2, int sizeL2)
 {
+    //int acarreo = 0;
+    //return addTwoNumbersHelper(l1, l2, sizeL1, sizeL2, acarreo);
     return nullptr;
 }
--- PAT_Parcial_Reposicion/Ejercicio02.cpp
+++ PAT_Parcial_Reposicion/Ejercicio02.cpp
@@ -2,5 +2,43 @@
 
 Node<char>* Ejercicio02::reverseKGroup(Node<char>* head, int k)
 {
-    return nullptr;
+    if (!head || k <= 1) {
+        return head;
+    }
+
+    Node<char>* nodo = new Node<char>; 
+    nodo->next = head;
+
+    Node<char>* prevprev = nodo;
+    Node<char>* prev = nullptr;
+    Node<char>* temp = nullptr;
+
+    Node<char>* slow = head;
+    Node<char>* fast = head;
+    int count = 0;
+    while (fast && fast->next) {
+        count += 2;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if (fast != nullptr)
+        count++;
+
+    while (count >= k) {
+        prev = prevprev->next;
+        temp = prev->next;
+
+        for (int i = 1; i < k; i++)
+        {
+            prev->next = temp->next;
+            temp->next = prevprev->next;
+            prevprev->next = temp;
+            temp = prev->next;
+        }
+
+        prevprev = prev;
+        count -= k;
+    }
+
+    return nodo->next;
 }
