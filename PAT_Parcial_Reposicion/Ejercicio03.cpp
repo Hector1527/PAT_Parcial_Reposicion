eb337f6103a2d87d7f83f4fa3d5e23a354633232
Hector1527 <hectorperezgonzalez09@gmail.com>
2023-11-11 17:11:43 -0600

    Ejercicio 2 completo

--- PAT_Parcial_Reposicion/Ejercicio03.cpp
+++ PAT_Parcial_Reposicion/Ejercicio03.cpp
@@ -2,5 +2,16 @@
 
 Node<int>* Ejercicio03::detectCycle(Node<int>* head)
 {
+	Node<int>* slow = head;
+	Node<int>* fast = head;
+
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (slow == fast)
+			return slow->next;
+	}
 	return nullptr;
 }
